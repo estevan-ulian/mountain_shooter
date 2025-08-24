e4b57e514c38415d659ee8d7b4ba6a4f20accfce
estevan-ulian <estevan.ulian@gmail.com>
2025-08-24 13:51:42 -0300

    Refactor EntityFactory to implement static method for entity creation

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from code.Background import Background
+from code.Const import WINDOW_WIDTH
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
-
-    def get_entity(self, entity_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+            case 'Level1Bg':
+                bg_list = []
+                for i in range(7):
+                    bg_list.append(Background(name=f'Level1Bg{i}', position=(0, 0)))
+                    bg_list.append(Background(name=f'Level1Bg{i}', position=(WINDOW_WIDTH, 0)))
+                return bg_list
