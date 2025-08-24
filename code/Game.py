2b8d9df90fbd5d3eafd60321d03473d50c016172
estevan-ulian <estevan.ulian@gmail.com>
2025-08-24 12:27:33 -0300

    Refactor Game class to use constants for window dimensions and clean up run method

--- code/Game.py
+++ code/Game.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
-
+from code.Const import WINDOW_WIDTH, WINDOW_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(
+            size=(WINDOW_WIDTH, WINDOW_HEIGHT))
 
     def run(self):
-        pygame.display.set_caption("Mountain Shooter")
-        menu = Menu(self.window)
-        menu.run()
-        # while True:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.QUIT:
-        #             pygame.quit()  # Close window
-        #             quit()  # end pygame
+        while True:
+            pygame.display.set_caption("Mountain Shooter")
+            menu = Menu(self.window)
+            menu.run()
+            pass
+        
